a524794b0e9a9c471ddb9dd003e98f2f12b2783d
Arunachalam <arunKumaran0133@gmail.com>
2023-07-11 06:35:38 +0000

    commited

---
+++ index.js
@@ -0,0 +1,17 @@
+const element = (
+  <div className="bg-container">
+    <h1>Congratulations</h1>
+    <div className="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p className="para">
+        Vishnu institute of Computer Education and Technology,
+        <br />
+        Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
